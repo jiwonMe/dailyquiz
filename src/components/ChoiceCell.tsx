45d48797344dba71bc9e9dd6ea60047068d25297
jiwonMe <park@jiwon.me>
2023-11-08 06:28:24 +0900

    update: choice cell

--- src/components/ChoiceCell.tsx
+++ src/components/ChoiceCell.tsx
@@ -1,5 +1,6 @@
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import { Description } from '../designs/typographys';
+import { FiCheckCircle, FiXCircle } from 'react-icons/fi';
 
 interface ChoiceCellProps {
   variant?: 'unselected' | 'selected' | 'correct' | 'wrong';
@@ -10,6 +11,16 @@ interface ChoiceCellProps {
 const ChoiceCell = ({ variant = 'unselected', ...props }: ChoiceCellProps) => {
   return (
     <ChoiceCellLayout variant={variant} {...props}>
+      <CellIcon>
+        {
+          {
+            unselected: '',
+            selected: '',
+            correct: <FiCheckCircle size={20} />,
+            wrong: <FiXCircle size={20} />,
+          }[variant]
+        }
+      </CellIcon>
       <Description>{props.children}</Description>
     </ChoiceCellLayout>
   );
@@ -17,9 +28,45 @@ const ChoiceCell = ({ variant = 'unselected', ...props }: ChoiceCellProps) => {
 
 export default ChoiceCell;
 
+const CellIcon = styled.div`
+  position: absolute;
+
+  left: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+
+  width: 24px;
+  height: 24px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// cell vibrate horizontally
+const wrongAnimation = keyframes`
+  0% {
+    transform: translateX(0px);
+  }
+  25% {
+    transform: translateX(-4px);
+  }
+  50% {
+    transform: translateX(0px);
+  }
+  75% {
+    transform: translateX(4px);
+  }
+  100% {
+    transform: translateX(0px);
+  }
+`;
+
 const ChoiceCellLayout = styled.div<{
   variant: 'unselected' | 'selected' | 'correct' | 'wrong';
 }>`
+  position: relative;
+
   display: flex;
   flex-direction: column;
 
@@ -37,11 +84,11 @@ const ChoiceCellLayout = styled.div<{
     switch (variant) {
     default:
     case 'unselected':
-      return `
+      return css`
           background-color: ${theme.colors.gray50};
         `;
     case 'selected':
-      return `
+      return css`
           background-color: ${theme.colors.gray300};
           border: 2px solid ${theme.colors.gray500};
 
@@ -50,24 +97,28 @@ const ChoiceCellLayout = styled.div<{
           }
         `;
     case 'correct':
-      return `
+      return css`
           background-color: ${theme.colors.green100};
           border: 2px solid ${theme.colors.green500};
-          
+          color: ${theme.colors.green500};
+
           ${Description} {
             color: ${theme.colors.green500};
             font-weight: 600;
           }
         `;
     case 'wrong':
-      return `
+      return css`
           background-color: ${theme.colors.red100};
           border: 2px solid ${theme.colors.red500};
+          color: ${theme.colors.red500};
 
           ${Description} {
             color: ${theme.colors.red500};
             font-weight: 600;
           }
+
+          animation: ${wrongAnimation} 0.2s ease-in-out;
         `;
     }
   }}
