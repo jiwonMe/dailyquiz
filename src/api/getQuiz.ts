ad653b7c20fdd133810d4722f6f72e546c58e86b
jiwonMe <park@jiwon.me>
2023-11-10 22:06:59 +0900

    fix: api response error

--- src/api/getQuiz.ts
+++ src/api/getQuiz.ts
@@ -9,10 +9,17 @@ import { Quiz } from '../types/Quiz';
 export const getQuiz = async (): Promise<Quiz> => {
   const response = await fetch(
     `https://opentdb.com/api.php?amount=10&category=${Math.floor(
-      Math.random() * 33
+      Math.random() * 32 + 1
     )}&difficulty=medium&type=multiple`
   );
+
   const data = (await response.json()) as TDBApiResponse;
+
+  // if response code is 1, then there is no quiz
+  if (data.response_code === 1) {
+    return getQuiz();
+  }
+
   const title = data.results[0].category;
   const description = data.results[0].question;
 
